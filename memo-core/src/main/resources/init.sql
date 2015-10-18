e0ec0db59b1c53480cf891a8e5e56145f35736ab
roshane <roshanemadusanka@gmail.com>
2015-10-18 22:57:11 +0530

    cleaned up unnecessary spring config files

--- memo-core/src/main/resources/init.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO 'USER' (name,age) values ("Nilanka Kumara",23);
-INSERT INTO 'USER' (name,age) values ("Dilukshan silva",43);
-INSERT INTO 'USER' (name,age) values ("Dushan fernando",50);
--- memo-core/src/test/resources/init.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Customer (name,age) values ('Nilanka Kumara',23);
-INSERT INTO Customer (name,age) values ('Dilukshan silva',43);
-INSERT INTO Customer (name,age) values ('Dushan fernando',50);
